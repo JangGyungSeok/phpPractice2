12afaad432aa578ab99f5b770ebe0366a4a374ad
JKS <juskus0310@naver.com>
2020-07-08 08:40:56 +0900

    bookPractice 프로젝트 추가 (실전 PHP 웹 프로그래밍 내용을 다룰예정)

---
+++ bookPractice/routes/web.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('auth/login',function(){
+    $cridentials = [
+        'email' => 'JKS.google.com',
+        'password' => 'password',
+    ];
+
+    if(! auth()->attempt($cridentials)){
+        return "로그인이 부정확하다.";
+    }
+
+    return redirect('protected');
+});
+
+Route::get('protected', function(){
+    dump(session() ->all());
+    if(!auth() ->check()){
+        return "누구세요?";
+    }
+
+    return '하이'.auth()->user()->name;
+});
+
+Route::get('auth/logout',function(){
+    auth() ->logout();
+
+    return "또봐요";
+});
